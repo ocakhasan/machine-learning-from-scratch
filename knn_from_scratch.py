563711a2617f1424aa32e997418bdb2f4fb42618
Hasan Ocak <ocakhasan99@gmail.com>
2020-11-04 10:52:43 +0300

    added some exception conditions such as if k > number of input etc

--- knn_from_scratch.py
+++ knn_from_scratch.py
@@ -3,6 +3,10 @@ import numpy as np
 class KNN:
 
     def __init__(self, k=2, distance_metric="eucledian"):
+        
+        if distance_metric != "eucledian" and distance_metric != "manhattan":
+            raise Exception("Distance metric must be eucledian or manhattan")
+
         self.k =k 
         self.distance_metric = distance_metric
         self.X = None
@@ -10,32 +14,35 @@ class KNN:
 
 
     def fit(self, X, y):
+
         if len(X.shape) != 2 or len(y.shape) != 2:
             raise Exception("Input Shape must be 2D, your input shape is {}, try array.reshape(-1, 1)".format(X.shape))
-        
-        if self.k > self.X.shape[0]:
-            raise Exception("Your nearest neighbour count is more than your input. Try add more data or decrease the number of neighbours")
 
         self.X = X
         self.y = y
+        
+
+        if self.k > self.X.shape[0]:
+            raise exception("your nearest neighbour count is more than your input. try add more data or decrease the number of neighbours")
 
         print("Model fitted to program")
 
-    def predict(self, point):
+    def predict(self, prediction_X):
 
-        point = np.array(point)
+        prediction_X = np.array(prediction_X)
 
-        if len(point.shape) != len(self.X.shape):
+        if len(prediction_X.shape) != len(self.X.shape):
             raise Exception("Your prediction array must be same shape with your Input Size.")
-        if point.shape[1] != self.X.shape[1]:
+
+        if prediction_X.shape[1] != self.X.shape[1]:
             raise Exception("Your input size for prediction is not equal with your training data.",
-                                 "Your input shape is {} and your prediction array shape is {}".format(self.X.shape, point.shape))
+                                 "Your input shape is {} and your prediction array shape is {}".format(self.X.shape, prediction_X.shape))
 
         if self.distance_metric == "eucledian":
-           distances = np.sqrt(np.sum(np.square(self.X - point), axis=1))
+           distances = np.sqrt(np.sum(np.square(self.X - prediction_X), axis=1))
         
         elif self.distance_metric == "manhattan":
-            distances = np.sum(np.abs(self.X - y), axis=1)
+            distances = np.sum(np.abs(self.X - prediction_X), axis=1)
 
         indices = np.argsort(distances)[:self.k]                    #get the indices of the closest points to given input
         near_labels = self.y[indices]                               #get the labels of closest points to given input
