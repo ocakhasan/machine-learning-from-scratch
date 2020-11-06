fee62437056a35806d3f3bb09bd5ad792167abec
Hasan Ocak <ocakhasan99@gmail.com>
2020-11-07 00:20:47 +0300

    apply testing on every data and add a showcase of iris data

--- knn_from_scratch.py
+++ knn_from_scratch.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 class KNN:
 
@@ -30,36 +32,52 @@ class KNN:
     def predict(self, prediction_X):
 
         prediction_X = np.array(prediction_X)
+        predictions = []
 
-        if len(prediction_X.shape) != len(self.X.shape):
-            raise Exception("Your prediction array must be same shape with your Input Size.")
+        for point in prediction_X:
 
-        if prediction_X.shape[1] != self.X.shape[1]:
-            raise Exception("Your input size for prediction is not equal with your training data.",
-                                 "Your input shape is {} and your prediction array shape is {}".format(self.X.shape, prediction_X.shape))
+            try:
+                point = point.reshape((1, self.X.shape[1]))
+            except:
+                raise Exception("your prediction test data is not same size with your input data."
+                                "Your training data has {} features and your test data shape is {}".format(X_train.shape[1],point.shape))
 
-        if self.distance_metric == "eucledian":
-           distances = np.sqrt(np.sum(np.square(self.X - prediction_X), axis=1))
-        
-        elif self.distance_metric == "manhattan":
-            distances = np.sum(np.abs(self.X - prediction_X), axis=1)
+            if len(point.shape) != len(self.X.shape):
+                raise Exception("Your prediction array must be same shape with your Input Size.")
 
-        indices = np.argsort(distances)[:self.k]                    #get the indices of the closest points to given input
-        near_labels = self.y[indices]                               #get the labels of closest points to given input
-        counters =  np.unique(near_labels, return_counts=True)      #get the counts of every labels 
-        labels = counters[0]
-        values = counters[1] 
-        max_ind_label = np.argsort(values)[-1]                      #get the index of label which has more closest points
-        prediction = labels[max_ind_label]                          #return the label 
+            if point.shape[1] != self.X.shape[1]:
+                print(point.shape[0], self.X.shape[1])
+                raise Exception("Your input size for prediction is not equal with your training data.",
+                                    "Your input shape is {} and your prediction array shape is {}".format(self.X.shape, prediction_X.shape))
 
-        return prediction 
+            if self.distance_metric == "eucledian":
+                distances = np.sqrt(np.sum(np.square(self.X - point), axis=1))
+            
+            elif self.distance_metric == "manhattan":
+                distances = np.sum(np.abs(self.X - point), axis=1)
+
+            indices = np.argsort(distances)[:self.k]                    #get the indices of the closest points to given input
+            near_labels = self.y[indices]                               #get the labels of closest points to given input
+            counters =  np.unique(near_labels, return_counts=True)      #get the counts of every labels 
+            labels = counters[0]
+            values = counters[1] 
+            max_ind_label = np.argsort(values)[-1]                      #get the index of label which has more closest points
+            prediction = labels[max_ind_label]    
+            predictions.append(prediction)                      #return the label 
+
+        return predictions 
 
         
         
 
 if __name__ == "__main__":
     model = KNN(3, "eucledian")
-    X = np.array([[4, 2], [3, 4], [1, 1], [2, 3]])
-    y = np.array([[10], [2], [3], [10]])
-    model.fit(X, y)
-    print(model.predict([[2, 3]]))
+    data = load_iris()
+    X = data.data
+    y = data.target.reshape(-1, 1) 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    model.fit(X_train, y_train)
+    predictions = np.array(model.predict(X_test)).reshape(-1, 1)
+    is_predictions_true = np.array(predictions == y_test)
+    accuracy  = np.sum(is_predictions_true) / len(y_test)
+    print("Accuracy is ", accuracy)
