d7fa0ed38cd7cba3e204ce45e0f0cee9342abe05
Hasan Ocak <ocakhasan99@gmail.com>
2020-11-07 23:00:06 +0300

    add some comment

--- lineer-regression-scratch.py
+++ lineer-regression-scratch.py
@@ -6,28 +6,27 @@ from sklearn.metrics import mean_squared_error
 class LineerRegression():
 
     def fit(self, X, y, learning_rate=0.01):
+
         theta_dimension = (1 + X.shape[1], 1)
         self.THETA = np.zeros(theta_dimension, dtype=np.float32)
         self.learning_rate = learning_rate
 
-        xzeros = np.ones([X.shape[0], 1], dtype=X.dtype)
-
+        xzeros = np.ones([X.shape[0], 1], dtype=X.dtype)                        #Add meaningless 0 data for some convention
         X = np.concatenate((xzeros, X), axis=1)
 
         for x_m, y_m in zip(X, y):
     
-            hypothesis_x = self.hypothesis(x_m, self.THETA)
-
-            loss = self.loss_function(hypothesis_x, y_m)
-
-            THETA = self.update_parameters(self.THETA, hypothesis_x, x_m, y_m, self.learning_rate)
+            hypothesis_x = self.hypothesis(x_m, self.THETA)                              #calculate hypothesis
+            loss = self.loss_function(hypothesis_x, y_m)                                 #calculate loss function
+            THETA = self.update_parameters(self.THETA, hypothesis_x,                    
+                                            x_m, y_m, self.learning_rate)                 #Update THETA parameters
 
         print("Training is done")
         print("The parameters are \n", THETA)
     
     def predict(self, X_test):
 
-        xzeros = np.ones([X_test.shape[0], 1], dtype=X_test.dtype)
+        xzeros = np.ones([X_test.shape[0], 1], dtype=X_test.dtype)                      #Add meanningless 0 data again
         X_test = np.concatenate((xzeros, X_test), axis=1)
         predictions = []
 
@@ -54,10 +53,10 @@ class LineerRegression():
             return Updated_THETA
 
 def main():
-    X, y = make_regression(n_samples = 250, n_features=3, noise=20, random_state=0, bias=50)
+    X, y = make_regression(n_samples = 250, n_features=3, noise=20, random_state=0, bias=50)                #Create some dummy regression data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
     print("Data is created")
-    model  = LineerRegression()
+    model  = LineerRegression()                             
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     print(mean_squared_error(predictions, y_test))
